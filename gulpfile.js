943042e546ce4ffa37bab63e45d61d458b3592e2
Rob McDiarmid <rmcdiarmid@konradgroup.com>
2016-12-22 14:40:52 -0500

    added cache busting and global sass

--- gulpfile.js
+++ gulpfile.js
@@ -14,19 +14,16 @@ let del = require('del');
 let prodMode = argv.prod;
 
 //Build TODOs
-//global Sass
 //vendor css
 //vendor JS. e.g. moment
-//cache bust js and css
 //css source maps
 //js source maps
-//clean dist on build
 //Live reload
 
-function styles() {
+function componentStyles() {
     let sassOptions = {outputStyle: prodMode ? 'compressed' : 'nested'};
 
-    return gulp.src('src/**/*.scss')
+    return gulp.src(['src/**/*.scss', '!src/globalSass/**'])
         .pipe(sass(sassOptions).on('error', sass.logError))
         .pipe(gulp.dest('src'));
 }
@@ -36,63 +33,91 @@ function ngc() {
 }
 
 let rollUpBundle;
-function rollupApp() {
-    let devPlugins = [
-        nodeResolve({jsnext: true, module: true}),
-        commonjs({include: 'node_modules/rxjs/**'})
-    ];
+let rollupApp = gulp.series(
+    function cleanGlobalJs() {return del('dist/app*.js')},
+    function buildRollupApp() {
+        let devPlugins = [
+            nodeResolve({jsnext: true, module: true}),
+            commonjs({include: 'node_modules/rxjs/**'})
+        ];
 
-    let prodPlugins = [...devPlugins, uglify()];
+        let prodPlugins = [...devPlugins, uglify()];
 
-    let rollupConfig = {
-        entry: 'src/main.js',
-        cache: rollUpBundle,
-        plugins: prodMode ? prodPlugins : devPlugins,
-        onwarn: function (msg) {
-            if (!msg.includes("The 'this' keyword is equivalent to 'undefined' at the top level of an ES module, and has been rewritten")) {
-                console.error(msg);
+        let rollupConfig = {
+            entry: 'src/main.js',
+            cache: rollUpBundle,
+            plugins: prodMode ? prodPlugins : devPlugins,
+            onwarn: function (msg) {
+                if (!msg.includes("The 'this' keyword is equivalent to 'undefined' at the top level of an ES module, and has been rewritten")) {
+                    console.error(msg);
+                }
             }
-        }
-    };
+        };
 
-    return rollup(rollupConfig)
-        .then((bundle) => {
-            rollUpBundle = bundle;
+        return rollup(rollupConfig)
+            .then((bundle) => {
+                rollUpBundle = bundle;
 
-            return bundle.write({
-                format: 'iife',
-                dest: `dist/app-${Date.now().toString(36)}.js`
+                return bundle.write({
+                    format: 'iife',
+                    dest: `dist/app-${Date.now().toString(36)}.js`
+                });
             });
-        });
-}
+    }
+);
 
-function globalJs() {
-    return gulp.src([
-        'node_modules/core-js/client/shim.min.js',
-        'node_modules/zone.js/dist/zone.min.js'
-    ])
-        .pipe(concat('global.js'))
-        .pipe(rev())
-        .pipe(gulp.dest('dist'));
-}
+let globalJs = gulp.series(
+    function cleanGlobalJs() {return del('dist/global*.js')},
+    function BuildGlobalJs() {
+
+        return gulp.src([
+            'node_modules/core-js/client/shim.min.js',
+            'node_modules/zone.js/dist/zone.min.js'
+        ])
+            .pipe(concat('global.js'))
+            .pipe(rev())
+            .pipe(gulp.dest('dist'));
+    }
+);
+
+let globalSass = gulp.series(
+    function cleanGlobalSass() {return del('dist/global*.css')},
+    function BuildGlobalSass() {
+        let sassOptions = {outputStyle: prodMode ? 'compressed' : 'nested'};
+
+        return gulp.src('src/globalSass/global.scss')
+            .pipe(sass(sassOptions).on('error', sass.logError))
+            .pipe(rev())
+            .pipe(gulp.dest('dist'));
+    }
+);
+
 
 function index() {
-    let srcStream = gulp.src(['dist/global*.js', 'dist/app*.js'], {read: false});
+    let srcStream = gulp.src(['dist/global*.js', 'dist/app*.js', 'dist/global*.css'], {read: false});
 
     return gulp.src('src/index.html')
         .pipe(inject(srcStream, {addRootSlash: false, ignorePath: 'dist'}))
         .pipe(gulp.dest('dist'));
 }
 
-gulp.task(styles);
-gulp.task(ngc);
-gulp.task('rollupApp', gulp.series(() => del('dist/app-*.js'), rollupApp));
-gulp.task('globalJs', gulp.series(() => del('dist/global-*.js'), globalJs));
-//TODO: encapsulate the clean and rollup in an function so we don't need to call it as a string
-let appJs = gulp.series(styles, ngc, 'rollupApp');
-gulp.task(index);
+function clean() {
+    return del('dist');
+}
 
-gulp.task('default', gulp.series(gulp.parallel(appJs, globalJs), index, function watch() {
-    gulp.watch('src/**/*.ts', appJs);
-    gulp.watch('src/**/*.scss', styles);
+gulp.task('componentStyles', componentStyles);
+gulp.task('ngc', ngc);
+gulp.task('rollupApp', rollupApp);
+gulp.task('globalJs', globalJs);
+gulp.task('globalSass', globalSass);
+gulp.task('index', index);
+
+let appJs = gulp.series(componentStyles, ngc, rollupApp);
+let build = gulp.series(clean, gulp.parallel(appJs, globalJs), index);
+
+gulp.task('default', gulp.series(build, function watch() {
+    gulp.watch('src/**/*.ts', gulp.series(ngc, rollupApp, index));
+    gulp.watch(['src/**/*.scss', '!src/globalSass/**'], gulp.series(componentStyles, index));
+    gulp.watch('src/globalSass/**/*.scss', gulp.series(globalSass, index));
+    gulp.watch('src/index.html', index);
 }));
